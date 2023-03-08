4bd5d651c2cda0b4a2d564664ea1b31b7072e83e
DESKTOP-HMIO5U8\James <james@jcaholdings.com>
2023-03-07 20:40:09 -0500

    changed mount

--- index.js
+++ index.js
@@ -33,7 +33,7 @@ const app = express();
 app.use('/public', express.static(path.join(__dirname, '/public')));
 
 // Serve the Parse API on the /parse URL prefix
-const mountPath = process.env.PARSE_MOUNT || '/parse';
+const mountPath = process.env.PARSE_MOUNT || '/';
 if (!test) {
   const api = new ParseServer(config);
   app.use(mountPath, api);
