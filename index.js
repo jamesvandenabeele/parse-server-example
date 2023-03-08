4842c41b8b28bb99fdc781359b9d761053d16afc
DESKTOP-HMIO5U8\James <james@jcaholdings.com>
2023-03-07 20:21:50 -0500

    updated server config

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ const config = {
   cloud: process.env.CLOUD_CODE_MAIN || __dirname + '/cloud/main.js',
   appId: process.env.APP_ID || 'rBakd2Db5rupITJjE2SXq66a24rW4q6cIaGB9j4C',
   masterKey: process.env.MASTER_KEY || '', //Add your master key here. Keep it secret!
-  serverURL: process.env.SERVER_URL || 'http://localhost:1337/parse', // Don't forget to change to https if needed
+  serverURL: process.env.SERVER_URL || 'https://dashboard.heroku.com/apps/reverie-production', // Don't forget to change to https if needed
   liveQuery: {
     classNames: ['Posts', 'Comments'], // List of classes to support for query subscriptions
   },
