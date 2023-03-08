b0d99b89182208d0202d78cd28d58cd85c0d4aa0
DESKTOP-HMIO5U8\James <james@jcaholdings.com>
2023-03-07 20:27:20 -0500

    More config updates

--- index.js
+++ index.js
@@ -13,13 +13,13 @@ if (!databaseUri) {
   console.log('DATABASE_URI not specified, falling back to localhost.');
 }
 const config = {
-  databaseURI: databaseUri || 'mongodb://test_user:Svw@zACG_6kPy38@cluster-4s2z08c3.9ffxb.mongodb.net/?retryWrites=true&w=majority',
+  databaseURI: databaseUri || 'mongodb://test_user:Svw@zACG_6kPy38@cluster-4s2z08c3.9ffxb.mongodb.net',
   cloud: process.env.CLOUD_CODE_MAIN || __dirname + '/cloud/main.js',
   appId: process.env.APP_ID || 'rBakd2Db5rupITJjE2SXq66a24rW4q6cIaGB9j4C',
   masterKey: process.env.MASTER_KEY || '', //Add your master key here. Keep it secret!
   serverURL: process.env.SERVER_URL || 'https://dashboard.heroku.com/apps/reverie-production', // Don't forget to change to https if needed
   liveQuery: {
-    classNames: ['Posts', 'Comments'], // List of classes to support for query subscriptions
+    classNames: ['Alarm', 'Bedstate','ComfortSetting','Diagnostics','Role','Routine','Session','User','UserInfo','WarrentyInfo'], // List of classes to support for query subscriptions
   },
 };
 // Client-keys like the javascript key or the .NET key are not necessary with parse-server
